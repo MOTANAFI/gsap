7f3249c3fcae3e83b35a3459303332c299a0d4c6
motanafi <twaha2030@gmail.com>
2025-06-25 21:12:35 +0300

    gsap hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // import GsapBox from "./components/GsapBox"
-
-import GsapHoverBox from "./components/GsapHoverBox"
-
+// import GsapHoverBox from "./components/GsapHoverBox"
 // import GsapTimeline from "./components/GsapTimeline"
-
 // import GsapStaggerSimple from "./components/StaggerSimple"
-
 // import GsapStaggerBox from "./components/GsapStaggerBox"
+// import GsapClickBox from "./components/GsapClickBox"
+
+import GsapChallenge from "./components/GsapChallenge"
+
 
 
 function App() {
@@ -18,7 +18,10 @@ function App() {
   {/* <GsapStaggerBox /> */}
   {/* <GsapStaggerSimple /> */}
   {/* <GsapTimeline /> */}
-  <GsapHoverBox />
+  {/* <GsapHoverBox /> */}
+  {/* <GsapClickBox /> */}
+  <GsapChallenge />
+  
     
     </>
   )
---
+++ src/components/GsapChallenge.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GsapChallenge() {
+  return (
+    <div className='h-screen flex items-center justify-center bg-gray-50'>GsapChallenge</div>
+  )
+}
+
+export default GsapChallenge
